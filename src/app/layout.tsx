5a01572a238a718fe897afd7cc083b085f182a12
louisdevzz <vohuunhan1310@gmail.com>
2025-01-01 19:24:22 +0700

    feat: add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Toaster } from 'react-hot-toast'
 import { Space_Grotesk } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/react"
+
 
 const spaceGrotesk = Space_Grotesk({ subsets: ['latin'] })
 
@@ -22,6 +24,7 @@ export default function RootLayout({
         <div className="mx-auto max-w-[1440px] px-4 sm:px-6 lg:px-8">
           <Providers>{children}</Providers>
           <Toaster position="top-center" />
+          <Analytics />
         </div>
       </body>
     </html>
